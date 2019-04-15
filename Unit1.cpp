543c03b65c852f13eb40555c9904a71357428c73
TendaiRondof <47823089+TendaiRondof@users.noreply.github.com>
2019-04-15 08:35:05 +0200

    Update

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,30 +8,41 @@
 #pragma package(smart_init)
 #pragma resource "*.fmx"
 TForm1 *Form1;
+
+float DesinWidth;
+float DesinHeight;
+
+void define_size(void){
+	DesinWidth = 360;
+	DesinHeight = 740;
+}
+#pragma startup define_size
+
+
 //---------------------------------------------------------------------------
 
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
-	Forms::TScreen* Screen= new Forms::TScreen(Owner);
-	TSize ScreenSize = Screen->Size();
 	TControl *current_comp;
+
+	Label1->Text = Form1->ClientWidth;
+	Label2->Text = DesinWidth;
+
 	float XFactor,YFactor;
 
-	XFactor = float(ScreenSize.Width)/Form1->ClientWidth;
-	YFactor = float(ScreenSize.Height)/Form1->ClientHeight;
-
-	Label1->Text = "W:"+String(XFactor);
-	Label2->Text = "H:"+String(YFactor);
+	XFactor = Form1->ClientWidth/DesinWidth;
+	YFactor = Form1->ClientHeight/DesinHeight;
 
 	for (int comp=0; comp < Form1->ComponentCount; comp++) {
 		current_comp = dynamic_cast<TControl*>(Form1->Components[comp]);
-		if (current_comp == 0) {
+		if (current_comp != 0) {
 			current_comp->Position->X *= XFactor;
 			current_comp->Position->Y *= YFactor;
+			current_comp->Width *= XFactor;
+			current_comp->Height *= YFactor;
 			current_comp->Repaint();
 		}
 	}
-	delete Screen;
 }
 //---------------------------------------------------------------------------
