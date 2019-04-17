78ede33c182a0782121039c4777acc9f42eaaaa7
TendaiRondof <47823089+TendaiRondof@users.noreply.github.com>
2019-04-17 13:23:53 +0200

    Update

--- Unit1.cpp
+++ Unit1.cpp
@@ -9,14 +9,8 @@
 #pragma resource "*.fmx"
 TForm1 *Form1;
 
-float DesinWidth;
-float DesinHeight;
-
-void define_size(void){
-	DesinWidth = 360;
-	DesinHeight = 740;
-}
-#pragma startup define_size
+float DesinWidth = 360; 	//Your disigning  width
+float DesinHeight = 740;	//Your disigning height
 
 
 //---------------------------------------------------------------------------
@@ -25,9 +19,6 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
 	TControl *current_comp;
-
-	Label1->Text = Form1->ClientWidth;
-	Label2->Text = DesinWidth;
 
 	float XFactor,YFactor;
 
